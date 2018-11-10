180efc9c7c3adffea59b82236cbfdec5b6e757bf
muhwezideo <aggrey256@gmail.com>
2018-11-10 15:06:23 +0300

    fix auth_user_model in settings,fix error in userprofile model, create migration and migrate

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,47 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin,BaseUserManager
+from django.contrib.auth.models import PermissionsMixin, BaseUserManager
 # Create your models here.
+
 
 class UserProfileManager(BaseUserManager):
     """" Helps django work with custom user model"""
-    def create_user(self,email,name,password=None):
+
+    def create_user(self, email, name, password=None):
         """" creates a new user object"""
         if not email:
             raise ValueError('Users must have an email')
-        email=self.normalize_email(email)
+        email = self.normalize_email(email)
         # convert email to lowercase
-        user=self.model(email=email,name=name)
+        user = self.model(email=email, name=name)
         # creates user object
-        user.set_password(password) 
+        user.set_password(password)
         # saves password
         user.save(using=self._db)
         # saves user
-        return user 
-    
-    def create_superuser(self,email,name,password):
+        return user
+
+    def create_superuser(self, email, name, password):
         """ creates superuser /admin with given details"""
-        user=self.create_user(email,name,password)
-        user.is_superuser=True
-        user.is_staff=True
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
-        return user 
+        return user
 
 
-class UserProfile(AbstractBaseUser,PermissionsMixin):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """ Respresent User Profile in the system """
-    email=models.EmailField(max_length=255,unique=True)
-    name=models.CharField(max_length=255)
-    is_active=models.BooleanField(default=True)
-    is_staff=models=models.BooleanField(default=False)
-    objects=UserProfileManager()
-    USERNAME_FIELD='email'
-    REQUIRED_FIELDS=['name']
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
     """ helper functions"""
+
     def get_full_names(self):
         """ used to get a users full name"""
         return self.name
+
     def get_short_name(self):
         """ Used to get a users short name."""
         return self.name
--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'profiles_api',
+    'profiles_api.apps.ProfilesApiConfig',
     'rest_framework',
     'rest_framework.authtoken'
 ]
@@ -121,4 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-AUTH_USER_MODELS='profiles_api.UserProfile'
+AUTH_USER_MODEL='profiles_api.UserProfile'
