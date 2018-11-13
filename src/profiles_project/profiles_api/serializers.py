4579af6705784481c71853a3552e9768d4f4ba25
muhwezideo <aggrey256@gmail.com>
2018-11-13 17:16:30 +0300

    add fix

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -9,17 +9,16 @@ class HelloSerializer(serializers.Serializer):
 
 class UserProfileSerializer(serializers.Serializer):
     """ Seriailzer for user Profile object"""
-    id=serializers.IntegerField(read_only=True)
-    email=serializers.EmailField(max_length=255)
-    name=serializers.CharField(max_length=10)
-    password=serializers.CharField(max_length=100,write_only=True)
-   
+    id = serializers.IntegerField(read_only=True)
+    email = serializers.EmailField(max_length=255)
+    name = serializers.CharField(max_length=10)
+    password = serializers.CharField(max_length=100, write_only=True)
 
     class Meta:
         model = models.UserProfile
         # fields = ('id', 'email', 'name', 'password')
         # extra_kwargs = {'password': {'write_only': True}}
-        
+
         # makes password field readonly
 
     def create(self, validated_data):
@@ -31,3 +30,21 @@ class UserProfileSerializer(serializers.Serializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+    def update(self, request, pk=None,*args, **kwargs):
+        self.queryset = models.UserProfile.objects.all()
+        instance = self.queryset.get(pk=kwargs.get('pk'))
+        serializer = self.serializer_class(
+            instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def partial_update(self, request, *args, **kwargs):
+        self.queryset = models.UserProfile.objects.all()
+        instance = self.queryset.get(pk=kwargs.get('pk'))
+        serializer = self.serializer_class(
+            instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
