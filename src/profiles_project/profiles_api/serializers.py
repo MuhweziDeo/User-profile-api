e4e1e4498bdb41075c3eb5fa7264379b6271acec
muhwezideo <aggrey256@gmail.com>
2018-11-14 13:12:17 +0300

    add serializer for user profile

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -44,3 +44,10 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
          """
     
+class ProfileFeedSerializer(serializers.ModelSerializer):
+	""" serializer for profile feed items"""
+
+	class Meta:
+		model=models.ProfileFeedItem
+		fields=('id','user_profile','status_text','create_on')
+		extra_kwargs={'user_profile':{'readonly':True}}
